44cde8ccfcebdb5df8eca2cc4b4a1eddae448415
Tomas Malio <tomasmalio@gmail.com>
2019-03-25 12:22:19 -0300

    First pull of GoMovil Deportes

---
+++ extensions/metaTags/metaTags.php
@@ -0,0 +1,30 @@
+<?php
+    /**
+     <meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta itemprop="name" content="" />
+    <meta itemprop="description" content="" />
+    <meta itemprop="url" content="" />
+    <meta itemprop="image" content="" />
+    <meta name="schema:title" content="" />
+    <meta name="schema:description" content="" />
+    <meta name="schema:image" content="" />
+    <meta property="og:title" content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="" />
+    <meta property="og:site_name" content="" />
+    <meta property="fb:app_id" content="" />
+    <meta name="twitter:card" content="" />
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:description" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:site" content="" />
+    <meta name="twitter:creator" content="" />
+    <meta name="twitter:image" content="" />
+    <link itemprop="sameAs" id="sitelink-profile-twitter" href="" />
+    <link itemprop="sameAs" id="sitelink-profile-facebook" href="" />
+    <link rel="canonical" href="" />
+     */
+?>
