3fc1c10cb7464fedf94aeeea1790d2341d98c5cd
Tomas Malio <tomasmalio@gmail.com>
2019-03-29 12:03:09 -0300

    Adding index.php & Composer & Twig

--- extensions/matchFeatured/matchFeatured.php
+++ extensions/matchFeatured/matchFeatured.php
@@ -1,1 +1,17 @@
-$styles = array();
+<?php
+
+?>
+<!-- Featured Content -->
+<section class="featured-content">
+	<div class="featured-news">
+		<a href="#" title="Noticia del día">
+			<div class="content" style="background-image: url('https://s3.us-east-2.amazonaws.com/bitel/deportes/nota/jhon-viafara-deportivo-cali-festejo-gol-afp_1000x500.jpg');">
+				<h3>
+					<span>Noticias del día</span>
+				</h3>
+				<h2>Los giros en el destino de Viáfara</h2>
+			</div>
+		</a>
+	</div>
+</section>
+<!-- Eof Featured Content -->
