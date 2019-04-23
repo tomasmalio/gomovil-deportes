58a43fe6703862a15bb008fa335a0ee29fbcdd87
Tomas Malio <tomasmalio@gmail.com>
2019-04-23 14:44:44 -0300

    Adding script for change owner in asset

--- gomovil-install.sh
+++ gomovil-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 username=$(cat /etc/passwd | grep Apache | awk -F':' '{print $1}')
 if [ -n "$username" ]; then
-	chown -R $username:$username assets/
+	sudo -u root chown -R $username:$username assets/
 fi
