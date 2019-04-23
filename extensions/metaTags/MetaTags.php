0da96a29caf2d2654f3e26f0a8a7a7da2ec234aa
Tomas Malio <tomasmalio@gmail.com>
2019-04-23 11:53:53 -0300

    Trying to fix metatags error name

---
+++ extensions/metaTags/MetaTags.php
@@ -0,0 +1,46 @@
+<?php
+	/**
+	 * MetaTags
+	 */
+	class MetaTags extends Widgets {
+		//public $title;
+		public $general = [
+			'title' 			=> '',
+			'description' 		=> '',
+			'keywords' 			=> '',
+			'image'				=> '',
+			'url'				=> '',
+		];
+		
+		public $facebook = [
+			'title' 			=> '',
+			'description' 		=> '',
+			'image'				=> '',
+			'url'				=> '',
+			'app_id'			=> '',
+			'type'				=> '',
+			'site'				=> '',
+			'siteLink'			=> 'https://www.facebook.com/',
+		];
+
+		public $twitter = [
+			'title' 			=> '',
+			'description' 		=> '',
+			'image'				=> '',
+			'url'				=> '',
+			'site'				=> '',
+			'siteLink'			=> 'https://www.twitter.com/',
+			'creator'			=> '',
+			'card'				=> '',
+		];
+
+		public function renderView () {
+			return Widgets::renderViewHtml([
+					'general' 	=> $this->general,
+					'twitter' 	=> $this->twitter,
+					'facebook' 	=> $this->facebook,
+				]
+			);
+		}
+	}
+?>
