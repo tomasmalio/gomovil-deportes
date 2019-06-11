3be0c765d9d0bba80054eca8a003e8154ce31a18
Tomas Malio <tomasmalio@gmail.com>
2019-06-11 13:00:49 -0300

    Adding more action for sh

--- gomovil-install.sh
+++ gomovil-install.sh
@@ -3,5 +3,6 @@ username=$(cat /etc/passwd | grep Apache | awk -F':' '{print $1}')
 if [ -n "$username" ]; then
 	sudo -u root chown -R $username:$username assets/
 	sudo -u root chown -R $username:$username css/
+	sudo -u root chown -R $username:$username less/
 	sudo -u root chown -R $username:$username extensions/
 fi
