85beb5322570e57cf12652bae65c1297b53365b4
Tomas Malio <tomasmalio@gmail.com>
2019-06-11 12:29:10 -0300

    Adding for conditions for composer

--- gomovil-install.sh
+++ gomovil-install.sh
@@ -2,5 +2,6 @@
 username=$(cat /etc/passwd | grep Apache | awk -F':' '{print $1}')
 if [ -n "$username" ]; then
 	sudo -u root chown -R $username:$username assets/
+	sudo -u root chown -R $username:$username css/
 	sudo -u root chown -R $username:$username extension/
 fi
