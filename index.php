5949d6e2f18204f6e4fcc017d3902a5df40951ec
Tomas Malio <tomasmalio@gmail.com>
2019-06-11 14:33:43 -0300

    Display errors no

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 	require_once __DIR__.'/bootstrap.php';
-	ini_set('display_errors', 1);
+	/*ini_set('display_errors', 1);*/
+
 	/* Declare of extensions directory */
 	define('EXTENSIONS_URL', '/extensions', true);
 
