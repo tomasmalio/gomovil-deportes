25bb1b680ade61724d9b0b176eac7e071d5dd747
Tomas Malio <tomasmalio@gmail.com>
2019-06-12 15:32:19 -0300

    Adding tmp for creation

--- gomovil-install.sh
+++ gomovil-install.sh
@@ -4,4 +4,5 @@ if [ -n "$username" ]; then
 	sudo -u root chown -R $username:$username assets/
 	sudo -u root chown -R $username:$username css/
 	sudo -u root chown -R $username:$username less/
+	sudo -u root chown -R $username:$username tmp/
 fi
