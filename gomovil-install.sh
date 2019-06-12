20622b48fa24ab9fa27b5c4e19ab1dbfa2f6bae3
Tomas Malio <tomasmalio@gmail.com>
2019-06-12 14:38:58 -0300

    Changing permissions

--- gomovil-install.sh
+++ gomovil-install.sh
@@ -4,5 +4,4 @@ if [ -n "$username" ]; then
 	sudo -u root chown -R $username:$username assets/
 	sudo -u root chown -R $username:$username css/
 	sudo -u root chown -R $username:$username less/
-	sudo -u root chown -R $username:$username tmp/
 fi
