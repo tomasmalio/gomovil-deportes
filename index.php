053bee0c5cf0faea71c0c556d45ca3eb16b2159e
Tomas Malio <tomasmalio@gmail.com>
2019-06-18 11:15:45 -0300

    Check error

--- index.php
+++ index.php
@@ -151,8 +151,8 @@
 		}
 		unset($keywords[$i]);
 	}
-	// print_r($keywords);
-	// print_r($keywordsChange);
+	print_r($keywords);
+	print_r($keywordsChange);
 	/**********************************
 	 * 			MENU
 	 **********************************/
