501249a8ec20ab45aaaba42ccff9391aaa9f2478
Tomas Malio <tomasmalio@gmail.com>
2019-06-22 22:11:35 -0300

    Adding validate for content empty in Assets

--- src/Assets.php
+++ src/Assets.php
@@ -13,8 +13,11 @@
 			$filenameContent 	= ROOTPATH. '/less/content.' . $name . '.less';
 			$globalLess 		= ROOTPATH. '/less/styles.' . $name . '.less';
 			$globalCss			= ROOTPATH. '/css/styles.' . $name . '.min.css';
-			if (!file_exists($globalLess))
+			if (!file_exists($globalLess)) {
 				echo "ACAAAAA";
+			} else {
+				echo file_exists($globalLess);
+			}
 			if ($params['modify_status'] == '1' || file_exists($globalLess) != true || file_get_contents($globalLess) == '') {
 				try {
 					$handle 			= fopen($filename, 'w') or die('Cannot open file:  '. $filename); 
