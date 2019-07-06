5dab0d7a8b87bd32b6a63a9b7dec4de20f1b61f8
Tomas Malio <tomasmalio@gmail.com>
2019-07-06 20:33:22 -0300

    Adding a install for creating

---
+++ install.php
@@ -0,0 +1,12 @@
+<?php
+	echo 'Installing software';
+	echo 'Downloading composer';
+	shell_exec('php -r "copy(\'https://getcomposer.org/installer\', \'composer-setup.php\');"');
+	shell_exec('php -r "copy(\'https://getcomposer.org/installer\', \'composer-setup.php\');"');
+	shell_exec('php -r "if (hash_file(\'sha384\', \'composer-setup.php\') === \'48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5\') { echo \'Installer verified\'; } else { echo \'Installer corrupt\'; unlink(\'composer-setup.php\'); } echo PHP_EOL;"');
+	shell_exec('php composer-setup.php');
+	shell_exec('php -r "unlink(\'composer-setup.php\');"');
+	echo 'Installing PHP Composer...';
+	shell_exec('php composer install');
+	echo 'Installation ended';
+?>
