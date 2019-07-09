fcf3cc5c2c56c9242631422167a74530dd87da02
Tomas Malio <tomasmalio@gmail.com>
2019-07-09 18:28:54 -0300

    Adding Top trends social extension

--- extensions/toptrendsSocial/views/viewToptrendsSocial.php
+++ extensions/toptrendsSocial/views/viewToptrendsSocial.php
@@ -37,6 +37,7 @@
 								<div class="author-username">@<?=$social['screen_name']?></div>
 							</div>
 						</div>
+						<?php /*<p class="text"><?=Widgets::convertSocialLinks(Widgets::makeLinks($social['text']), strtolower($social['origen']))?></p>*/?>
 						<p class="text"><?=$social['text']?></p>
 					</div>
 					<div class="card-footer">
