b9a1f24b1c9d5e65b604cd7886272a3e7d484094
Tomas Malio <tomasmalio@gmail.com>
2019-07-18 15:01:50 -0300

    [Menu] Adding modal for 18 years

--- templates/menu.inc.php
+++ templates/menu.inc.php
@@ -9,7 +9,7 @@
 		<li><a href="/{{ item.url }}">{{ item.title }}</a></li>
 		{% else %}
 		{% set array = array|merge([{url: item.url}]) %}
-		<li href="#{{ item.url }}" data-toggle="modal">{{ item.title }}</li>
+		<li><a href="#{{ item.url }}" data-toggle="modal">{{ item.title }}</a></li>
 		{% endif %}
 		{% else %}
 		<li><a href="/{{ item.url }}">{{ item.title }}</a></li>
