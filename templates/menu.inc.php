666d8a2dfbe94a5706008743ed99c947766b4720
Tomas Malio <tomasmalio@gmail.com>
2019-07-18 14:32:23 -0300

    [Menu] Adding modal for 18 years

--- templates/menu.inc.php
+++ templates/menu.inc.php
@@ -1,20 +1,22 @@
-<ul class="menu-responsive">
-	<li class="logo"><a href="/"><img src="/images/header/{{ logo }}" name="logo" title="" alt=""></a></li>
-	{% for item in menu %}
+<div class="sb-slidebar sb-right">
+	<ul class="menu-responsive">
+		<li class="logo"><a href="/"><img src="/images/header/{{ logo }}" name="logo" title="" alt=""></a></li>
+		{% for item in menu %}
 
-	{% set array = [] %}
-	{% if item.age_control is not empty %}
-	{% if age_control == true %} 
-	<li><a href="/{{ item.url }}">{{ item.title }}</a></li>
-	{% else %}
-	{% set array = array|merge([{url: item.url}]) %}
-	<li><a href="#{{ item.url }}" data-toggle="modal">{{ item.title }}</a></li>
-	{% endif %}
-	{% else %}
-	<li><a href="/{{ item.url }}">{{ item.title }}</a></li>
-	{% endif %}
-	{% endfor %}
-</ul>
+		{% set array = [] %}
+		{% if item.age_control is not empty %}
+		{% if age_control == true %} 
+		<li><a href="/{{ item.url }}">{{ item.title }}</a></li>
+		{% else %}
+		{% set array = array|merge([{url: item.url}]) %}
+		<li><a href="#{{ item.url }}" data-toggle="modal">{{ item.title }}</a></li>
+		{% endif %}
+		{% else %}
+		<li><a href="/{{ item.url }}">{{ item.title }}</a></li>
+		{% endif %}
+		{% endfor %}
+	</ul>
+</div>
 {% if array is not empty %}
 {% for item in array %}
 <div id="{{ item.url }}" class="modal hide fade modal-age-control" tabindex="-1">
