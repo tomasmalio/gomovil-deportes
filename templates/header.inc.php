cc220f02685628fce53635da65e7a7b67c2536c6
Tomas Malio <tomasmalio@gmail.com>
2019-07-18 12:56:32 -0300

    [Menu] Adding modal for 18 years

--- templates/header.inc.php
+++ templates/header.inc.php
@@ -111,14 +111,16 @@
 	</div>
 </header>
 <!-- Eof Header -->
-<div id="las-nenas" class="modal hide fade" tabindex="-1" data-width="760">
-	<div class="modal-header">
-	<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-		<h3>Responsive</h3>
-	</div>
-	<div class="modal-body">aca</div>
-	<div class="modal-footer">
-		<button type="button" data-dismiss="modal" class="btn">Close</button>
-		<button type="button" class="btn btn-primary">Save changes</button>
+<div id="las-nenas" class="modal hide fade modal-age-control" tabindex="-1" data-width="760">
+	<div class="molal-content">
+		<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+			<h3>Responsive</h3>
+		</div>
+		<div class="modal-body">aca</div>
+		<div class="modal-footer">
+			<button type="button" data-dismiss="modal" class="btn">Close</button>
+			<button type="button" class="btn btn-primary">Save changes</button>
+		</div>
 	</div>
 </div>
