815bab0a5fd48431fbb53cf46d19479a6868ba11
Tomas Malio <tomasmalio@gmail.com>
2019-07-18 14:08:46 -0300

    [Menu] Adding modal for 18 years

--- templates/menu.inc.php
+++ templates/menu.inc.php
@@ -1,6 +1,37 @@
 <ul class="menu-responsive">
 	<li class="logo"><a href="/"><img src="/images/header/{{ logo }}" name="logo" title="" alt=""></a></li>
 	{% for item in menu %}
-	<li><a href="{{ item.url }}">{{ item.title }}</a></li>
+	{% set ageControl = '' %}
+	{% if item.age_control is not empty %}
+	{% set ageControl = 'id=modal-age-control' %}
+	{% if age_control == true %} 
+	<li><a href="/{{ item.url }}">{{ item.title }}</a></li>
+	{% else %}
+	<li {{ ageControl }}><a href="#{{ item.url }}" data-toggle="modal">{{ item.title }}</a></li>
+	<div id="{{ item.url }}" class="modal hide fade modal-age-control" tabindex="-1">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+			</div>
+			<div class="modal-body">
+				<div class="image-age-control">
+					<img src="/images/header/modal/imge-age-control.svg" name="agecontrol" title="" alt="">
+				</div>
+				<p>Para acceder a esta sección, tiene que ser mayor de 18 años.</p>
+			</div>
+			<div class="modal-footer">
+				<form action="/" method="post">
+					<input type="hidden" name="url" value="{{ item.url }}">
+					<input type="hidden" name="ageControl" value="1">
+					<button type="button" class="btn-close-age-control" data-dismiss="modal" aria-hidden="true">Volver</button>
+					<button type="submit" name="submit" class="btn-age-control">Soy mayor de 18 años</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	{% endif %}
+	{% else %}
+	<li><a href="/{{ item.url }}">{{ item.title }}</a></li>
+	{% endif %}
 	{% endfor %}
 </ul>
