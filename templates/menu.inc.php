ed78933103bbf8b8134ee4a18b66534cafee3630
Tomas Malio <tomasmalio@gmail.com>
2019-07-18 14:34:14 -0300

    [Menu] Adding modal for 18 years

--- templates/menu.inc.php
+++ templates/menu.inc.php
@@ -17,28 +17,3 @@
 		{% endfor %}
 	</ul>
 </div>
-{% if array is not empty %}
-{% for item in array %}
-<div id="{{ item.url }}" class="modal hide fade modal-age-control" tabindex="-1">
-	<div class="modal-content">
-		<div class="modal-header">
-			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-		</div>
-		<div class="modal-body">
-			<div class="image-age-control">
-				<img src="/images/header/modal/imge-age-control.svg" name="agecontrol" title="" alt="">
-			</div>
-			<p>Para acceder a esta sección, tiene que ser mayor de 18 años.</p>
-		</div>
-		<div class="modal-footer">
-			<form action="/" method="post">
-				<input type="hidden" name="url" value="{{ item.url }}">
-				<input type="hidden" name="ageControl" value="1">
-				<button type="button" class="btn-close-age-control" data-dismiss="modal" aria-hidden="true">Volver</button>
-				<button type="submit" name="submit" class="btn-age-control">Soy mayor de 18 años</button>
-			</form>
-		</div>
-	</div>
-</div>
-{% endfor %}
-{% endif %}
