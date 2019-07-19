591c4abe4e071493ff46d95dfbf05443cd2eb1f8
Tomas Malio <tomasmalio@gmail.com>
2019-07-19 12:22:53 -0300

    [Extension][Next Matches Football] Adding position for next matches

--- extensions/Widgets.php
+++ extensions/Widgets.php
@@ -146,8 +146,6 @@
 						if (isset($this->options['scripts'][1])){
 							$var = $this->options['scripts'][1]['content'];
 							if (isset($content['slider_position'])) {
-								echo $content['slider_position'];
-								exit;
 								$var = str_replace("initialSlide: '{@initialSlide},'", "initialSlide: ".($content['slider_position']-1).",", $var);
 							} else {
 								$var = str_replace("initialSlide: '{@initialSlide},'", "", $var);
