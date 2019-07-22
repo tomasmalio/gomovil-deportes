ced1c571a67f7ec17f0e878d1c013dcb55effc77
Tomas Malio <tomasmalio@gmail.com>
2019-07-22 09:55:53 -0300

    [Extension][Match Football Details] Fixing error

--- extensions/matchFootballDetails/model/ModelMatchFootballDetails.php
+++ extensions/matchFootballDetails/model/ModelMatchFootballDetails.php
@@ -81,10 +81,10 @@
 				$match = self::getMatchDetails($params['match']);
 				unset($match->trends);
 
-				echo "<pre>";
-				print_r($match);
-				echo "</pre>";
-				exit;
+				// echo "<pre>";
+				// print_r($match);
+				// echo "</pre>";
+				// exit;
 				// echo "<pre>";
 				// print_r(Widgets::multiRenameKey($match, $this->mappingName['wrong'], $this->mappingName['verify']));
 				// echo "</pre>";
