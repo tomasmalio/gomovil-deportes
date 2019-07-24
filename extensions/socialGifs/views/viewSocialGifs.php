2c7e9c060f165c7305f2d4f99fcbb1e666530a0f
Tomas Malio <tomasmalio@gmail.com>
2019-07-24 15:08:13 -0300

    [Extension][News Sports GoMovil] Changing shadow

--- extensions/socialGifs/views/viewSocialGifs.php
+++ extensions/socialGifs/views/viewSocialGifs.php
@@ -11,7 +11,8 @@
 		<div class="col-12">
 			<div class="post-gif">
 				<?php if (IS_MOBILE) {?>
-					<a href="whatsapp://send?text=Te invitó a descargar este GIF: <?=$gif['images']['downsized_large']['url']?>" data-action="share/whatsapp/share" target="_blank"><img src="<?=$gif['images']['downsized_large']['url'];?>" name="<?=$gif['slug']?>" width="<?=$gif['images']['downsized_large']['width'];?>" height="<?=$gif['images']['downsized_large']['height'];?>"></a>
+					<?php /*<a href="whatsapp://send?text=Te invitó a descargar este GIF: <?=$gif['images']['downsized_large']['url']?>" data-action="share/whatsapp/share" target="_blank"><img src="<?=$gif['images']['downsized_large']['url'];?>" name="<?=$gif['slug']?>" width="<?=$gif['images']['downsized_large']['width'];?>" height="<?=$gif['images']['downsized_large']['height'];?>"></a>*/?>
+					<div class="shareBtn"><img src="<?=$gif['images']['downsized_large']['url'];?>" name="<?=$gif['slug']?>" width="<?=$gif['images']['downsized_large']['width'];?>" height="<?=$gif['images']['downsized_large']['height'];?>"></div>
 				<?php } else {?>
 					<img src="<?=$gif['images']['downsized_large']['url'];?>" name="<?=$gif['slug']?>" width="<?=$gif['images']['downsized_large']['width'];?>" height="<?=$gif['images']['downsized_large']['height'];?>">
 				<?php }?>
@@ -23,3 +24,30 @@
 		?>
 	</div>
 </div>
+
+<script>
+	const shareBtn = document.querySelector('.share-btn');
+	const ogBtnContent = shareBtn.textContent;
+	const title = document.querySelector('h1').textContent;
+	const url = document.querySelector('link[rel=canonical]') && document.querySelector('link[rel=canonical]').href || window.location.href;
+	shareBtn.addEventListener('click', () => {
+		if (navigator.share) {
+			navigator.share({
+				title, url
+			}).then(() => {
+				showMessage(shareBtn, 'Thanks! 😄');
+			}).catch(err => {
+				showMessage(shareBtn, `Couldn't share 🙁`);
+			});
+		} else {
+			showMessage(shareBtn, 'Not supported 🙅&zwj;');
+		}
+	});
+
+	function showMessage(element, msg) {
+		element.textContent = msg;
+		setTimeout(() => {
+			element.textContent = ogBtnContent;
+		}, 2000);
+	}
+</script>
