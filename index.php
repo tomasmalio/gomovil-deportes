517c16c95a5532b3b4b09cd08373eaef9d8616c0
Tomas Malio <tomasmalio@gmail.com>
2019-07-31 15:37:44 -0300

    [AMP] Fixing error in styles

--- index.php
+++ index.php
@@ -503,9 +503,9 @@
 
 		// $assetsStyle	= '<![CDATA[';
 		$assetsStyle	= '';
+		// $assetsStyle 	.= $assetsConstructor->generateAssetsAmp($assets['css']);
+		// $assetsStyle 	.= $assetsConstructor->generateAssetsAmp($assetsGeneral['css']);
 		$assetsStyle 	.= $assetsConstructor->generateAssetsAmp([$globalStyle]);
-		$assetsStyle 	.= $assetsConstructor->generateAssetsAmp($assets['css']);
-		$assetsStyle 	.= $assetsConstructor->generateAssetsAmp($assetsGeneral['css']);
 		
 		$assetsStyle 	= str_replace('!important', '', $assetsStyle);
 		// $assetsStyle	.= ']]>';
