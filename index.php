a70c0f65033d721c461c167e9038cd15e96c8ce6
Tomas Malio <tomasmalio@gmail.com>
2019-08-01 15:47:45 -0300

    Creating services, modify the home, navigation changes

--- index.php
+++ index.php
@@ -501,7 +501,7 @@
 		array_push($assetsGeneral['css'], ['less/bootstrap-amp.min.css']);
 		// array_push($assetsGeneral['css'], ['https://use.fontawesome.com/releases/v5.7.0/css/all.css']);
 		array_push($assetsGeneral['css'], ['assets/slidebars/slidebars.min.css?v=20190701']);
-		//array_push($assetsGeneral['css'], ['assets/swiper/css/swiper.min.css?v=20190701']);
+		array_push($assetsGeneral['css'], ['assets/swiper/css/swiper.min.css?v=20190701']);
 		array_push($assetsGeneral['css'], [''.$globalStyle.'']);
 		// $assetsStyle	= '';
 		$assetsStyle	= '';
