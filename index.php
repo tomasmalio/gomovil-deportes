64a30040e5fcf6525881b6adcecf97628849f4f0
Tomas Malio <tomasmalio@gmail.com>
2019-08-01 11:49:36 -0300

    Fixing errors

--- index.php
+++ index.php
@@ -530,10 +530,10 @@
 		// print_r($htmlContent);
 		// exit;
 		/* Amp Remove Unused CSS */
-		// $ampRemoveUnusedCSS = new AmpRemoveUnusedCss();
-		// $ampRemoveUnusedCSS->process($htmlContent);
-		// echo $ampRemoveUnusedCSS->result();
-echo $htmlContent;
+		$ampRemoveUnusedCSS = new AmpRemoveUnusedCss();
+		$ampRemoveUnusedCSS->process($htmlContent);
+		echo $ampRemoveUnusedCSS->result();
+		// echo $htmlContent;
 		// $tmp = new AmpRemoveUnusedCss();
 		// $css_minified = $tmp->minify($assetsStyle);
 		// print_r($css_minified);
