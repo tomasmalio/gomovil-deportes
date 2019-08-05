bdc19bd6561fb0664d9c6fc81ed5508926f7f3cd
Tomas Malio <tomasmalio@gmail.com>
2019-08-05 15:33:25 -0300

    [phpFastCache] Adding php cache

--- gomovil-install.sh
+++ gomovil-install.sh
@@ -6,4 +6,5 @@ if [ -n "$username" ]; then
 	sudo -u root chown -R $username:$username less/
 	sudo -u root chown -R $username:$username tmp/
 	sudo -u root chown -R $username:$username cache/
+	sudo -u root chown -R $username:$username files/
 fi
