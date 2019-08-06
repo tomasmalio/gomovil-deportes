d9c658b886bc1f47d7a3aa85adc3c003e16d54b0
Tomas Malio <tomasmalio@gmail.com>
2019-08-06 17:04:02 -0300

    [phpFastCache] Adding php cache

--- index.php
+++ index.php
@@ -139,20 +139,22 @@
 	 * Client definitions
 	 */
 	if (!$CachedClient->isHit()) {
-		echo 'FIRST LOAD // WROTE OBJECT TO CACHE // RELOAD THE PAGE AND SEE // ';
+		echo "no cargo";
+		// Charge info client to cache
 		$db->prepare("select c.*, cy.code as country_code, cy.name as country_name, l.value as language, z.zone_name from client c, country cy, language l, zone z where url like '%" . $domain . "%' and c.country_id = cy.id and c.language_id = l.id and c.zone_id = z.id and c.status = 1");
 		$db->execute();
-		$client = $db->fetch();
+		$clientSql = $db->fetch();
 
-		$CachedClient->set($client)->expiresAfter(100);//in seconds, also accepts Datetime
-		$InstanceCache->save($CachedClient); // Save the cache item just like you do with doctrine and entities
-		
+		$CachedClient->set($clientSql)->expiresAfter(100); // In seconds, also accepts Datetime
+		$InstanceCache->save($CachedClient); // Save the cache item just like you do with doctrine and entities	
 	} else {
-		echo 'SECOND LOAD';	
+		echo "cargo";
+		// Loading client info from cache
 	}
-	print_r($CachedClient->get());
+	$client = $CachedClient->get();
+	print_r($client);
 
-
+	// Delete everything
 	//$InstanceCache->clear();
 	exit;
 
