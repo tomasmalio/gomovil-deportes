9640ab95961c7cd03ff4b4ef4eafa867d26a5993
Tomas Malio <tomasmalio@gmail.com>
2019-08-12 15:43:09 -0300

    [Extension][News Google] Changing model

--- extensions/newsGoogle/model/ModelNewsGoogle.php
+++ extensions/newsGoogle/model/ModelNewsGoogle.php
@@ -91,17 +91,17 @@
 						$newTitle .= $arrayTitle[$i];
 					}
 					echo $newTitle;
-					echo rtrim($title, ' - ').'<br>';
-					echo $title .'<br>';
-					$count = strlen($source);
-					echo $source . ' ' . $count. '<br>';
+					// echo rtrim($title, ' - ').'<br>';
+					// echo $title .'<br>';
+					// $count = strlen($source);
+					// echo $source . ' ' . $count. '<br>';
 					
 					$title = substr_replace($title, '', -(((strlen($source))/2)+3));
 					// $replace = [' - ', ' | '];
 					// $replacement = ['', '', ''];
 					$title = str_replace($replace, $replacement, $title);
 
-					echo $title .'<br>';
+					// echo $title .'<br>';
 
 					$count = '';
 				}
