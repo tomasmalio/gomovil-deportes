6c4b4beb517ff5936d2413dd0fe9349d3f14b365
Tomas Malio <tomasmalio@gmail.com>
2019-08-15 11:22:13 -0300

    Trying to add typePlatform variable

--- install-config.php
+++ install-config.php
@@ -1,11 +1,11 @@
 <?php 
 	//
-	$opts = getopt('type:');
+	$opts = getopt('t:');
 	print_r($opts);
 	$file = file_get_contents('index.php');
-	if (isset($opts['type']) && $opts['type'] != '') {
+	if (isset($opts['t']) && $opts['t'] != '') {
 		// echo $typePlatform;
-		$file = str_replace('{@TYPE_PRODUCTION}', $opts['type'], $file);
+		$file = str_replace('{@TYPE_PRODUCTION}', $opts['t'], $file);
 		// print_r($file);
 		file_put_contents($file);
 	} else {
