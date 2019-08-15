2311206e2ede93b36ee6067be5bccd6ff2d5f43a
Tomas Malio <tomasmalio@gmail.com>
2019-08-15 16:03:54 -0300

    [DB] Creating file

--- install-config.php
+++ install-config.php
@@ -2,14 +2,18 @@
 	/**
 	 * Creating the index for Production or Development
 	 */
-	$values = parse_str($argv[1]);
+	$values = parse_str($argv[2]);
 	print_r($argv);
-	$val = getopt("p:");
-	print_r($val);
-	$file = file_get_contents('include/db.php');
-	if ($val['p'] === 'prod') {
-		$file = str_replace('{@TYPE_PRODUCTION}', $val['p'], $file);
-	} else {
-		$file = str_replace('{@TYPE_PRODUCTION}', 'dev', $file);
-	}
-	file_put_contents('include/db.php', $file);
+	echo $argv[2];
+
+	$val = explode('&', $values);
+	
+	// $val = getopt("p:");
+	// print_r($val);
+	// $file = file_get_contents('include/db.php');
+	// if ($val['p'] === 'prod') {
+	// 	$file = str_replace('{@TYPE_PRODUCTION}', $val['p'], $file);
+	// } else {
+	// 	$file = str_replace('{@TYPE_PRODUCTION}', 'dev', $file);
+	// }
+	// file_put_contents('include/db.php', $file);
