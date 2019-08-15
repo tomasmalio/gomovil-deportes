c34328c0b93ba650e1a709967a733474000a7870
Tomas Malio <tomasmalio@gmail.com>
2019-08-15 16:13:33 -0300

    [DB] Creating file

--- install-config.php
+++ install-config.php
@@ -2,24 +2,18 @@
 	/**
 	 * Creating the index for Production or Development
 	 */
-	$values = parse_str($argv[2]);
-	print_r($argv);
-	print_r($values);
-	echo $argv[2];
-
+	//$values = parse_str($argv[2]);
 	$val = explode('&', $argv[2]);
-
-	print_r($val);
+	// print_r($val);
 	
+	$file = file_get_contents('include/db.php');
 
-	// $file = file_get_contents('include/db.php');
+	$file = str_replace('{@Username}', $val[0], $file);
+	$file = str_replace('{@Password}', $val[1], $file);
+	$file = str_replace('{@Dbname}', $val[2], $file);
+	$file = str_replace('{@Host}', $val[3], $file);
 
-	// $file = str_replace('{@Username}', $val[0], $file);
-	// $file = str_replace('{@Password}', $val[1], $file);
-	// $file = str_replace('{@Dbname}', $val[2], $file);
-	// $file = str_replace('{@Host}', $val[3], $file);
-
-	// file_put_contents('include/db.php', $file);
+	file_put_contents('include/db.php', $file);
 
 	// $val = getopt("p:");
 	// print_r($val);
