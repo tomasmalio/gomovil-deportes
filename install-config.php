14b33af2759f7de940d0b60271136fd7ac4b4dca
Tomas Malio <tomasmalio@gmail.com>
2019-08-15 15:44:51 -0300

    [DB] Creating file

--- install-config.php
+++ install-config.php
@@ -2,6 +2,8 @@
 	/**
 	 * Creating the index for Production or Development
 	 */
+	parse_str($argv[1]);
+	print_r($argv);
 	$val = getopt("p:");
 	print_r($val);
 	$file = file_get_contents('include/db.php');
