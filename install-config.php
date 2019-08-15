b64ce77349a5e9551a7f44939e74206da5491512
Tomas Malio <tomasmalio@gmail.com>
2019-08-15 10:51:00 -0300

    Trying to add typePlatform variable

--- install-config.php
+++ install-config.php
@@ -3,10 +3,10 @@
 	$file = file_get_contents('index.php');
 	if (isset($typePlatform) && $typePlatform != '') {
 		echo $typePlatform;
-		$newFile = str_replace('{@TYPE_PRODUCTION}', $typePlatform, $file);
-		file_put_contents($newFile, $file);
+		$file = str_replace('{@TYPE_PRODUCTION}', $typePlatform, $file);
+		file_put_contents($file);
 	} else {
 		$file = file_get_contents('index.php');
-		$newFile = str_replace('{@TYPE_PRODUCTION}', 'development', $file);
-		file_put_contents($newFile, $file);
+		$file = str_replace('{@TYPE_PRODUCTION}', 'development', $file);
+		file_put_contents($file);
 	}
