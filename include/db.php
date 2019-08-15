9d0a9e7ec4ded0adf982c66ced4141bf1546fc72
Tomas Malio <tomasmalio@gmail.com>
2019-08-15 16:07:56 -0300

    [DB] Creating file

--- include/db.php
+++ include/db.php
@@ -1,7 +1,7 @@
 <?php
 	$db = new Db();
-	$db->setUsername('gomovil_db');
-	$db->setPassword('g0m0v1lc0');
-	$db->setDsn('mysql:dbname=gosports_dev;host=db.gomovil.co');
+	$db->setUsername('{@Username}');
+	$db->setPassword('{@Password}');
+	$db->setDsn('mysql:dbname={@Dbname};host={@Host}');
 	$db->connect();
 ?>
--- install-config.php
+++ install-config.php
@@ -8,6 +8,16 @@
 
 	$val = explode('&', $values);
 	
+
+	$file = file_get_contents('include/db.php');
+
+	$file = str_replace('{@Username}', $val[0], $file);
+	$file = str_replace('{@Password}', $val[1], $file);
+	$file = str_replace('{@Dbname}', $val[2], $file);
+	$file = str_replace('{@Host}', $val[3], $file);
+
+	file_put_contents('include/db.php', $file);
+
 	// $val = getopt("p:");
 	// print_r($val);
 	// $file = file_get_contents('include/db.php');
@@ -17,3 +27,4 @@
 	// 	$file = str_replace('{@TYPE_PRODUCTION}', 'dev', $file);
 	// }
 	// file_put_contents('include/db.php', $file);
+?>
