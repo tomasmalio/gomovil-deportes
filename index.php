f2b02d5134598fa1d8099dc25324fd5d8f458785
Tomas Malio <tomasmalio@gmail.com>
2019-08-21 12:32:59 -0300

    [Extension][Leagues List] Changing title fixing error in content

--- index.php
+++ index.php
@@ -530,7 +530,8 @@
 				 * We validate if the extensions has external content and from the client
 				 */
 				$extensionContent = null;
-				
+				print_r($extension['content']);
+				print_r($extension['external_content']);
 				if ((isset($extension['content']) && $extension['content'] != '') && (isset($extension['external_content']) && $extension['external_content'] != '')) {
 					$insert = utf8_encode(str_replace($keywords, $keywordsChange, $extension['content']));
 					$external = utf8_encode(str_replace($keywords, $keywordsChange, $externalContent['data']));
