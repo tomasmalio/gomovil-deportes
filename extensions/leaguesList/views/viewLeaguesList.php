e4ead9377dfeb35793fa8b6f7e31a529fb81eb68
Tomas Malio <tomasmalio@gmail.com>
2019-08-21 12:38:10 -0300

    [Extension][Leagues List] Changing title fixing error in content

--- extensions/leaguesList/views/viewLeaguesList.php
+++ extensions/leaguesList/views/viewLeaguesList.php
@@ -1,4 +1,3 @@
-<?php print_r($this)?>
 <section class="leagueslist">
 	<?php if (isset($content['content']['search']['display']) && $content['content']['search']['display']) {?>
 	<div class="content-league">
