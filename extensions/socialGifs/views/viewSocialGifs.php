6b825da85a665c8ad8121720b5c88100f01534f9
Tomas Malio <tomasmalio@gmail.com>
2019-08-21 10:04:32 -0300

    [Social Gifs] Share

--- extensions/socialGifs/views/viewSocialGifs.php
+++ extensions/socialGifs/views/viewSocialGifs.php
@@ -10,7 +10,7 @@
 		?>
 		<div class="col-12">
 			<div class="post-gif">
-				<a href="https://api.whatsapp.com/send?type=image&link=<?=$gif['images']['downsized_large']['url']?>" data-action="share/whatsapp/share" target="_blank"><img src="<?=$gif['images']['downsized_large']['url'];?>" name="<?=$gif['slug']?>" width="<?=$gif['images']['downsized_large']['width'];?>" height="<?=$gif['images']['downsized_large']['height'];?>"></a>
+				<a href="https://api.whatsapp.com/send?text=Te%20invitó%20a%20descargar%20este%20GIF:%20<?=$gif['images']['downsized_large']['url']?>" data-action="share/whatsapp/share" target="_blank"><img src="<?=$gif['images']['downsized_large']['url'];?>" name="<?=$gif['slug']?>" width="<?=$gif['images']['downsized_large']['width'];?>" height="<?=$gif['images']['downsized_large']['height'];?>"></a>
 			</div>
 		</div>
 		<?php 
