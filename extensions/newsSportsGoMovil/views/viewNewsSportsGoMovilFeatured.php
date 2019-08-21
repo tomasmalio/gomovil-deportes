9e84876caec9d3cbeca39ece6fef3b3f2208b4af
Tomas Malio <tomasmalio@gmail.com>
2019-08-21 11:33:38 -0300

    [Extension][News Sports GoMovil] Changing title fixing error in content

--- extensions/newsSportsGoMovil/views/viewNewsSportsGoMovilFeatured.php
+++ extensions/newsSportsGoMovil/views/viewNewsSportsGoMovilFeatured.php
@@ -16,14 +16,14 @@
 				$url .= strtolower($content['titles']['article']['default']);
 			}
 			$url .= '/';
-			$url .= $content['id'] .'/'. $this->normalizeString($content['title']);
+			$url .= $content['content']['id'] .'/'. $this->normalizeString($content['content']['title']);
 		?>	
 		<a href="<?=$url?>" title="<?=$news['title']?>">
-			<div class="content" style="background-image: url('<?=$content['image']?>');">
+			<div class="content" style="background-image: url('<?=$content['content']['image']?>');">
 				<h3>
-					<span><?=$content['label']?></span>
+					<span><?=$content['content']['label']?></span>
 				</h3>
-				<h2><?=$content['title']?></h2>
+				<h2><?=$content['content']['title']?></h2>
 			</div>
 		</a>
 	</div>
