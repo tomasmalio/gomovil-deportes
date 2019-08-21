f9005bd91f489ab66fb0d5d4ea63e5798216da2f
Tomas Malio <tomasmalio@gmail.com>
2019-08-21 10:40:01 -0300

    [Extension][News Sports GoMovil] Changing title fixing error in content

--- extensions/newsSportsGoMovil/views/viewNewsSportsGoMovilFeatured.php
+++ extensions/newsSportsGoMovil/views/viewNewsSportsGoMovilFeatured.php
@@ -1,6 +1,6 @@
 <!-- Featured Content -->
 <?php 
-	// print_r($content);
+	print_r($content);
 	// if (isset($content['titles']['news'][COUNTRY_CODE])) { echo strtolower($content['titles']['news'][COUNTRY_CODE]); } else { echo strtolower($content['titles']['news']['default']);}
 	// exit;
 ?>
