78605f9ca79811dc66374a0635883f77e3486d83
Tomas Malio <tomasmalio@gmail.com>
2019-08-22 10:23:05 -0300

    [Extension][Leagues List] Changing title fixing error in content

--- extensions/nextMatchesFootball/model/ModelNextMatchesFootball.php
+++ extensions/nextMatchesFootball/model/ModelNextMatchesFootball.php
@@ -64,6 +64,8 @@
 				$typeTournament = $params['type'];
 				$tournamentName = $params['tournaments'][$params['type']][$params['tournament']]['name']['default'];
 				$tournament = self::getTournaments($typeTournament, Widgets::normalizeString($tournamentName));
+				echo $typeTournament;
+				echo $tournamentName;
 				return Widgets::multiRenameKey(self::getFixture($tournament), $this->mappingName['wrong'], $this->mappingName['verify']);
 			}
 		}
