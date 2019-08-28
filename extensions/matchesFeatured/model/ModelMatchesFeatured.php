e4abd328fa11ab348816813fa9f3886ac126e6b3
Tomas Malio <tomasmalio@gmail.com>
2019-08-28 13:29:57 -0300

    Modify match featured

--- extensions/matchesFeatured/model/ModelMatchesFeatured.php
+++ extensions/matchesFeatured/model/ModelMatchesFeatured.php
@@ -4,9 +4,48 @@
 	 */
 	class ModelMatchesFeatured {
 		
-		public $urlFootball = 'http://biteldev.gomovil.co/api/partidos-destacados.json';
-		public $urlTennis= 'http://biteldev.gomovil.co/api/partidos-destacados.json';
-		public $urlNba = 'http://biteldev.gomovil.co/api/partidos-destacados.json';
+		private $urlFootball = 'http://biteldev.gomovil.co/api/partidos-destacados.json';
+		private $urlTennis= 'http://biteldev.gomovil.co/api/partidos-destacados.json';
+		private $urlNba = 'http://biteldev.gomovil.co/api/partidos-destacados.json';
+
+		// Mapping name JSON
+		private $mappingName = [
+			'wrong' 	=> [
+				'torneo',
+				'tipo',
+				'local',
+				'local_img',
+				'gol_local',
+				'penal_local',
+				'visitante',
+				'visitante_img',
+				'gol_visitante',
+				'penal_visitante',
+				'dia',
+				'hora',
+				'hora_inicio',
+				'hora_fin',
+				'estado',
+				'minuto',
+			],
+			'verify'	=> [
+				'tournament',
+				'type',
+				'local_name',
+				'local_image',
+				'local_gol',
+				'local_penalty',
+				'visit_name',
+				'visit_image',
+				'visit_gol',
+				'visit_penalty',
+				'day',
+				'hour',
+				'datetime_start',
+				'datetime_end',
+				'minute',
+			],
+		];
 
 		public function model ($params = []) {
 			switch ($params['type']) {
@@ -20,7 +59,7 @@
 					$json = file_get_contents($this->urlNba);
 					break;
 			}
-			return json_decode($json, true);
+			return $this->multiRenameKey(json_decode($json, true), $this->mappingName['wrong'], $this->mappingName['verify']);
 		}
 	}
 ?>
