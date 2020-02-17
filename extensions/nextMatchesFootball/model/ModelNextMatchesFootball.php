ab84cb05225d6f92a02c9f61584bedc18296f454
Tomás Malio <tomasmalio@gmail.com>
2020-02-17 20:49:27 -0300

    Changing things

--- extensions/nextMatchesFootball/model/ModelNextMatchesFootball.php
+++ extensions/nextMatchesFootball/model/ModelNextMatchesFootball.php
@@ -23,6 +23,7 @@
 				'nombre',
 				'torneo',
 				'campeonato',
+				'partido',
 				'fecha',
 				'local_escudo',
 				'local_resultado',
@@ -37,6 +38,7 @@
 				'name',
 				'tournament',
 				'championship',
+				'match',
 				'match_date',
 				'local_shield',
 				'local_result',
@@ -75,7 +77,7 @@
 		private function getFixture ($division, $championship) {
 			$array =  Widgets::multiRenameKey(json_decode(file_get_contents($this->json . '&user=' . $this->user . '&pwd=' . $this->pass . '&metodo=fixture&division='. $division .'&campeonato='. $championship), true), $this->mappingName['wrong'], $this->mappingName['verify']);
 			foreach ($array['fixture'] as $fixture) {
-				print_r($fixture);
+				print_r($fixture['partido']);
 			}
 			
 		}
