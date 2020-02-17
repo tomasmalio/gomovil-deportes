47d5d50dec785b135ed58113dbdb03c0a934482d
Tomás Malio <tomasmalio@gmail.com>
2020-02-17 09:33:31 -0300

    Changing things

--- include/db.php
+++ include/db.php
@@ -1,11 +1,8 @@
 <?php
 	use GoMovil\Db;
 	$db = new Db();
-	$db->setUsername('gomovil_db');
-	$db->setPassword('g0m0v1lc0');
-	$db->setDsn('mysql:dbname=gosports_dev;host=db.gomovil.co');
-	// $db->setUsername('{@Username}');
-	// $db->setPassword('{@Password}');
-	// $db->setDsn('mysql:dbname={@Dbname};host={@Host}');
+	$db->setUsername('{@Username}');
+	$db->setPassword('{@Password}');
+	$db->setDsn('mysql:dbname={@Dbname};host={@Host}');
 	$db->connect();
 ?>
