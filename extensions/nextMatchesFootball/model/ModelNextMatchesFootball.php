49597986fed28fc821ac95d12caef03ce223947d
Tomás Malio <tomasmalio@gmail.com>
2020-02-17 09:46:01 -0300

    Changing things

--- extensions/nextMatchesFootball/model/ModelNextMatchesFootball.php
+++ extensions/nextMatchesFootball/model/ModelNextMatchesFootball.php
@@ -35,8 +35,8 @@
 		public function model ($params = []) {
 			if ($params['type'] && $params['tournament']) {
 				$array = file_get_contents($this->tournaments . '&user=' . $this->user . '&pwd=' . $this->pass);
-				print_r($array);
-				$array = Widgets::multiRenameKey($array['torneos'], $this->mappingName['wrong'], $this->mappingName['verify']);
+				$array = json_decode($array->torneos);
+				$array = Widgets::multiRenameKey($array, $this->mappingName['wrong'], $this->mappingName['verify']);
 				print_r($array);
 			}
 			
