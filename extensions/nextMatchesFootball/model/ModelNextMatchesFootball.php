720266bf1f14f1c02e3c4ddb9a8f46f90f3bc43c
Tomás Malio <tomasmalio@gmail.com>
2020-02-17 22:32:01 -0300

    Changing things

--- extensions/nextMatchesFootball/model/ModelNextMatchesFootball.php
+++ extensions/nextMatchesFootball/model/ModelNextMatchesFootball.php
@@ -88,9 +88,9 @@
 				// 	echo "<br>";
 				// 	array_push($fixture, $fixture[$res['match']['match_date']]);
 				// }
-				array_push($fixture[$res['match']['match_date']], $res['match']);
+				// array_push($fixture[$res['match']['match_date']], $res['match']);
 			}
-			print_r($fixture);
+			// print_r($fixture);
 			
 		}
 
