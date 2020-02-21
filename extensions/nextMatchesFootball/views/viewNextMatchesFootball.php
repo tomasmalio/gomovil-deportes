d4a681af041a8b2d77e4a5ad0cf78cf3ff74f3ef
Tomás Malio <tomasmalio@gmail.com>
2020-02-21 11:24:37 -0300

    [Matches Football] changing view

--- extensions/nextMatchesFootball/views/viewNextMatchesFootball.php
+++ extensions/nextMatchesFootball/views/viewNextMatchesFootball.php
@@ -40,18 +40,23 @@
 						foreach ($competitionDays as $match) {
 							// print_r($match);
 							// exit;
-							/* Switch of each status */
-							switch ($match['status']) {
-								case 'Por comenzar':
-									$match['status'] = 'to start';
-									break;
-								case 'En vivo':
-									$match['status'] = 'live';
-									break;
-								case 'Finalizado':
-									$match['status'] = 'end';
-									break;	
+							if ($match['day'] == date('Y-m-d', now()) && $match['hour'] < date('H:i', now())) {
+								$match['status'] = 'to start';
+							} elseif ($match['day'] == date('Y-m-d', now()) && $match['hour'] > date('H:i', now())) {
+								$match['status'] = 'end';
 							}
+							/* Switch of each status */
+							// switch ($match['status']) {
+							// 	case 'Por comenzar':
+							// 		$match['status'] = 'to start';
+							// 		break;
+							// 	case 'En vivo':
+							// 		$match['status'] = 'live';
+							// 		break;
+							// 	case 'Finalizado':
+							// 		$match['status'] = 'end';
+							// 		break;	
+							// }
 					?>
 					<!-- Match -->
 					<li class="row match <?= $match['status']?>">
