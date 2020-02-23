e925ecff7c7659cf905151d4fefd123422aea545
Tomás Malio <tomasmalio@gmail.com>
2020-02-23 00:37:46 -0300

    Creating new extension Match Stats

--- extensions/matchStats/views/viewMatchStats.php
+++ extensions/matchStats/views/viewMatchStats.php
@@ -1,8 +1,7 @@
 <?php
 	$stats = $content['content'];
-	print_r($stats);
 ?>
-<div class="col-12 match-stats center-block" ng-show="showStats" center-block="" aria-hidden="false">
+<div class="col-12 match-stats center-block">
 	<!-- Match stats / Stats / Match / Match container -->
 	<div class="title-section">Estadísticas del partido</div>
 	<div class="stats-content">
@@ -12,13 +11,13 @@
 					<div class="row">
 						<div class="col-6">
 							<div class="progress-content-team d-none d-md-block">
-								<span class="progress-content-team-shield-left"><img ng-src="http://image.futmovil.com/56a26c0c6274c.png" title="Newell's" alt="Newell's" src="http://image.futmovil.com/56a26c0c6274c.png"></span>
+								<span class="progress-content-team-shield-left"><img src="http://image.futmovil.com/56a26c0c6274c.png" title="Newell's" alt="Newell's"></span>
 								<div class="progress-content-team-name left"><?=$stats['local_team']?></div>
 							</div>
 						</div>
 						<div class="col-6">
 							<div class="progress-content-team d-none d-md-block">
-								<span class="progress-content-team-shield-right"><img ng-src="http://image.futmovil.com/56a26ad5f32a7.png" title="Colón" alt="Colón" src="http://image.futmovil.com/56a26ad5f32a7.png"></span>
+								<span class="progress-content-team-shield-right"><img src="http://image.futmovil.com/56a26ad5f32a7.png" title="Colón" alt="Colón"></span>
 								<div class="progress-content-team-name right"><?=$stats['visit_team']?></div>
 							</div>
 						</div>
@@ -46,9 +45,9 @@
 				<!-- Goals -->
 				<div class="col-12 center-block progress-content">
 					<div class="row progress-result-info">
-						<div class="col-2 stats-progress-result left ng-binding" ng-bind="local_score"><?=$stats['local_gol']?></div>
+						<div class="col-2 stats-progress-result left" ng-bind="local_score"><?=$stats['local_gol']?></div>
 						<div class="col-8 stats-progress-description">Goles</div>
-						<div class="col-2 stats-progress-result right ng-binding" ng-bind="visitor_score"><?=$stats['visit_gol']?></div>
+						<div class="col-2 stats-progress-result right" ng-bind="visitor_score"><?=$stats['visit_gol']?></div>
 					</div>
 					<div class="row">
 						<div class="col-6 progress-bars-column left">
@@ -67,9 +66,9 @@
 				<!-- Total Shots on gol -->
 				<div class="col-12 center-block progress-content">
 					<div class="row progress-result-info">
-						<div class="col-2 stats-progress-result left ng-binding"><?=$stats['stats']['local_team']['shots_on_target']?></div>
+						<div class="col-2 stats-progress-result left"><?=$stats['stats']['local_team']['shots_on_target']?></div>
 						<div class="col-8 stats-progress-description">Tiros al arco</div>
-						<div class="col-2 stats-progress-result right ng-binding"><?=$stats['stats']['visit_team']['shots_on_target']?></div>
+						<div class="col-2 stats-progress-result right"><?=$stats['stats']['visit_team']['shots_on_target']?></div>
 					</div>
 					<div class="row">
 						<div class="col-6 progress-bars-column left">
@@ -88,9 +87,9 @@
 				<!-- Total Shots -->
 				<div class="col-12 center-block progress-content">
 					<div class="row progress-result-info">
-						<div class="col-2 stats-progress-result left ng-binding"><?=$stats['stats']['local_team']['shots']?></div>
+						<div class="col-2 stats-progress-result left"><?=$stats['stats']['local_team']['shots']?></div>
 						<div class="col-8 stats-progress-description">Tiros Totales</div>
-						<div class="col-2 stats-progress-result right ng-binding"><?=$stats['stats']['visit_team']['shots']?></div>
+						<div class="col-2 stats-progress-result right"><?=$stats['stats']['visit_team']['shots']?></div>
 					</div>
 					<div class="row">
 						<div class="col-6 progress-bars-column left">
@@ -109,9 +108,9 @@
 				<!-- Total Saves -->
 				<div class="col-12 center-block progress-content">
 					<div class="row progress-result-info">
-						<div class="col-2 stats-progress-result left ng-binding"><?=$stats['stats']['local_team']['tied']?></div>
+						<div class="col-2 stats-progress-result left"><?=$stats['stats']['local_team']['tied']?></div>
 						<div class="col-8 stats-progress-description">Atajadas</div>
-						<div class="col-2 stats-progress-result right ng-binding"><?=$stats['stats']['visit_team']['tied']?></div>
+						<div class="col-2 stats-progress-result right"><?=$stats['stats']['visit_team']['tied']?></div>
 					</div>
 					<div class="row">
 						<div class="col-6 progress-bars-column left">
@@ -130,9 +129,9 @@
 				<!-- Total Fouls-->
 				<div class="col-12 center-block progress-content">
 					<div class="row progress-result-info">
-						<div class="col-2 stats-progress-result left ng-binding">4</div>
+						<div class="col-2 stats-progress-result left">4</div>
 						<div class="col-8 stats-progress-description">Faltas cometidas</div>
-						<div class="col-2 stats-progress-result right ng-binding">4</div>
+						<div class="col-2 stats-progress-result right">4</div>
 					</div>
 					<div class="row">
 						<div class="col-6 progress-bars-column left">
@@ -151,9 +150,9 @@
 				<!-- Total Offside-->
 				<div class="col-12 center-block progress-content">
 					<div class="row progress-result-info">
-						<div class="col-2 stats-progress-result left ng-binding"><?=$stats['stats']['local_team']['offsides']?></div>
+						<div class="col-2 stats-progress-result left"><?=$stats['stats']['local_team']['offsides']?></div>
 						<div class="col-8 stats-progress-description">Posición adelantadas</div>
-						<div class="col-2 stats-progress-result right ng-binding"><?=$stats['stats']['visit_team']['offsides']?></div>
+						<div class="col-2 stats-progress-result right"><?=$stats['stats']['visit_team']['offsides']?></div>
 					</div>
 					<div class="row">
 						<div class="col-6 progress-bars-column left">
@@ -172,9 +171,9 @@
 				<!-- Total Yellow cards-->
 				<div class="col-12 center-block progress-content">
 					<div class="row progress-result-info">
-						<div class="col-2 stats-progress-result left ng-binding"><?=$stats['stats']['local_team']['yellow_card']?></div>
+						<div class="col-2 stats-progress-result left"><?=$stats['stats']['local_team']['yellow_card']?></div>
 						<div class="col-8 stats-progress-description">Tarjetas amarillas</div>
-						<div class="col-2 stats-progress-result right ng-binding"><?=$stats['stats']['visit_team']['yellow_card']?></div>
+						<div class="col-2 stats-progress-result right"><?=$stats['stats']['visit_team']['yellow_card']?></div>
 					</div>
 					<div class="row">
 						<div class="col-6 progress-bars-column left">
@@ -193,9 +192,9 @@
 				<!-- Total Red cards-->
 				<div class="col-12 center-block progress-content">
 					<div class="row progress-result-info">
-						<div class="col-2 stats-progress-result left ng-binding"><?=$stats['stats']['local_team']['red_card']?></div>
+						<div class="col-2 stats-progress-result left"><?=$stats['stats']['local_team']['red_card']?></div>
 						<div class="col-8 stats-progress-description">Tarjetas Rojas</div>
-						<div class="col-2 stats-progress-result right ng-binding"><?=$stats['stats']['visit_team']['red_card']?></div>
+						<div class="col-2 stats-progress-result right"><?=$stats['stats']['visit_team']['red_card']?></div>
 					</div>
 					<div class="row">
 						<div class="col-6 progress-bars-column left">
@@ -215,5 +214,4 @@
 		</div>
 	</div>
 	<!-- Eof match stats / Stats / Match / Match container -->
-	
 </div>
