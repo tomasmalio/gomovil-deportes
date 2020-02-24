7bb4ca0b0b2867a210bd9749846640109e3827eb
Tomás Malio <tomasmalio@gmail.com>
2020-02-23 23:56:34 -0300

    [Scorers List] Modify widget

--- extensions/nextMatches/views/viewNextMatches.php
+++ extensions/nextMatches/views/viewNextMatches.php
@@ -1,6 +1,7 @@
 <!-- Next matches-->
 <?php 
 	$nextMatches = $content['content'];
+	print_r($nextMatches);
 	$date = date('Y-m-d');
 
 	// Validate if we want to show a type of soccer and we've matches
