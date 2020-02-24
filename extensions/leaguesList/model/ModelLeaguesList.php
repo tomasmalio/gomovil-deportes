21b7612a7270c4840ab27d90de80fcdd6a3bd5d4
Tomás Malio <tomasmalio@gmail.com>
2020-02-24 09:14:00 -0300

    [League List] Changing url json

--- extensions/leaguesList/model/ModelLeaguesList.php
+++ extensions/leaguesList/model/ModelLeaguesList.php
@@ -26,6 +26,8 @@
 
 		public function model ($params = []) {
 			$array =  Widgets::multiRenameKey(json_decode(file_get_contents($this->json . '&user=' . $this->user . '&pwd=' . $this->pass . '&metodo=torneos'), true), $this->mappingName['wrong'], $this->mappingName['verify']);
+			print_r($array);
+			exit;
 			return $array['championship'];
 		}
 	}
