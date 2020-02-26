a7bf837a0f84684145f94338123bcc61fc59a0e0
Tomás Malio <tomasmalio@gmail.com>
2020-02-26 16:05:23 -0300

    [News API] Creating new widget

--- extensions/newsApi/model/ModelNewsApi.php
+++ extensions/newsApi/model/ModelNewsApi.php
@@ -38,12 +38,15 @@
 		public function model ($params = []) {
 			self::setEndPoint($params['endpoint']);
 			self::setSearch($params['search']);
+			echo "estoy aca";
 			print_r(self::getNews());
+			exit;
 		}
 
 		private function getNews () {
 			$json = @file_get_contents($this->json . $endpoint . '?q=' . $this->search . '&apiKey=' . $this->key . '&results='.$this->results);
 			if (strpos($http_response_header[0], "200")) {
+				echo 'aca';
 				return json_decode($json, true);
 			} else { 
 				return null;
