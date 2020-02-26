5cd381114cc1f8279deeb6b2d6197a496a3125f7
Tomás Malio <tomasmalio@gmail.com>
2020-02-26 16:02:10 -0300

    [News API] Creating new widget

---
+++ extensions/newsApi/NewsApi.php
@@ -0,0 +1,41 @@
+<?php
+	/**
+	 * News API
+	 */
+	class NewsApi extends Widgets {
+		// Content
+		public $content;
+
+		// Assets files
+		public $files = [
+			'style'		=> [''],
+			'js'		=> [],
+		];
+		// Options
+		public $options = [
+			'slider' => [
+				'desktop' 	=> [
+					'display' 		=> false,
+				],
+				'mobile' 	=> [
+					'display' 		=> false,
+				],
+			]
+		];
+
+		public function __construct($params = []) {
+			parent::__construct($params);
+		}
+
+		public function renderView () {
+			return parent::renderViewHtml([
+					'content'			=> $this->content,
+					'slider'			=> parent::slider(),
+					'pagination'		=> parent::sliderPagination(),
+					'navigation'		=> parent::sliderNavigation(),
+				],
+				$this->viewName
+			);
+		}
+	}
+?>
---
+++ extensions/newsApi/model/ModelNewsApi.php
@@ -0,0 +1,64 @@
+<?php
+	/**
+	 * Model News API
+	 * @source https://newsapi.org/
+	 * 
+	 * @author: tomas@plugty.com
+	 * @password: `%9\rd8@CJW+V5x"
+	 */
+	class ModelNewsApi {
+		// URL JSON
+		private $json = 'http://newsapi.org/v2/';
+		// API Key
+		private $key = '508ccd1edfab4f81b802f57879d68273';
+		// Endpoints
+		private $endpoint = 'everything';
+		// Search
+		private $search = '';
+		// Date from
+		private $from = '';
+		// Date to
+		private $to = '';
+		// Language
+		private $language = '';
+		// Results
+		private $results = 20;
+
+		// Mapping name JSON
+		private $mappingName = [
+			'wrong' 	=> [
+				
+			],
+			'verify'	=> [
+				
+			],
+		];
+
+		
+		public function model ($params = []) {
+			self::setEndPoint($params['endpoint']);
+			self::setSearch($params['search']);
+			print_r(self::getNews());
+		}
+
+		private function getNews () {
+			$json = @file_get_contents($this->json . $endpoint . '?q=' . $this->search . '&apiKey=' . $this->key . '&results='.$this->results);
+			if (strpos($http_response_header[0], "200")) {
+				return json_decode($json, true);
+			} else { 
+				return null;
+			}
+		}
+
+		public function setEndPoint ($endpoint) {
+			if (!empty($endpoint) && ($endpoint == 'everything' || $endpoint == 'top-headlines' || $endpoint == 'sources')) {
+				$this->endpoint = $endpoint;
+			}
+		}
+		public function setSearch ($search) {
+			if (!empty($search)) {
+				$this->search = $search;
+			}
+		}
+	}
+?>
