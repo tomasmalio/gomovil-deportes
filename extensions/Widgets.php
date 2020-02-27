ee89d1758bcd3fc980490e39801f921650c0bd37
Tomás Malio <tomasmalio@gmail.com>
2020-02-26 23:31:21 -0300

    [News API] Creating new widget

--- extensions/Widgets.php
+++ extensions/Widgets.php
@@ -42,6 +42,7 @@
 			unset($params['clientName']);
 			unset($params['id']);
 
+			print_r($params);
 			if (isset($params) && count($params) > 0) {
 				foreach ($params['data'] as $key => $value) {
 					if (property_exists(get_class($this), $key)) {
