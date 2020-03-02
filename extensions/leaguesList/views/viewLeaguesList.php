c34d71ec81ecb3ad38a069e219b0b466b6031473
Tomás Malio <tomasmalio@gmail.com>
2020-03-02 12:54:04 -0300

    [Leagues List] Editing

--- extensions/leaguesList/views/viewLeaguesList.php
+++ extensions/leaguesList/views/viewLeaguesList.php
@@ -1,3 +1,4 @@
+<?php print_r($content);exit;?>
 <section class="leagueslist">
 	<?php if (isset($content['content']['search']['display']) && $content['content']['search']['display']) {?>
 	<div class="content-league">
