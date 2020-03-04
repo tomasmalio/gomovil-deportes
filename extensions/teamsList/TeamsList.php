57fd7b5d0c05bfdf16e7796ace1c782c801115c4
Tomás Malio <tomasmalio@gmail.com>
2020-03-03 23:39:33 -0300

    [Teams List] Uploading new extension

---
+++ extensions/teamsList/TeamsList.php
@@ -0,0 +1,50 @@
+<?php
+	/**
+	 * Teams List
+	 */
+	class TeamsList extends Widgets {
+		// Content
+		public $content;
+
+		// Assets files
+		public $files = [
+			'style'		=> ['teams-list.less'],
+			'js'		=> [],
+		];
+		// Options
+		public $options = [
+			'slider' => [
+				'desktop' 	=> [
+					'display' 		=> true,
+					'pagination'	=> false,
+					'navigation'	=> true,
+				],
+				'mobile' 	=> [
+					'display' 		=> true,
+					'pagination'	=> false,
+					'navigation'	=> true,
+				],
+				'options' => [
+					'mousewheel' 	=> false
+				],
+				'dynamic' 	=> true,
+			]
+		];
+
+		public function __construct($params = []) {
+			parent::__construct($params);
+		}
+
+		public function renderView () {
+			return parent::renderViewHtml([
+					'content'			=> $this->content,
+					'slider'			=> parent::slider(),
+					'items'				=> parent::items(),
+					'pagination'		=> parent::sliderPagination(),
+					'navigation'		=> parent::sliderNavigation(),
+				],
+				$this->viewName
+			);
+		}
+	}
+?>
---
+++ extensions/teamsList/model/ModelTeamsList.php
@@ -0,0 +1,55 @@
+<?php
+	/**
+	 * Model Teams List
+	 */
+	class ModelTeamsList {
+		// URL JSON
+		private $json = 'http://gomovil.universofutbol.com/data.php?';
+		// User
+		private $user = 'gomovil';
+		// Password
+		private $pass = 'g0m0v1lc0&';
+
+		// Slider position
+		private $sliderPosition = 0;
+
+		// Mapping name JSON
+		private $mappingName = [
+			'wrong' 	=> [
+				'nombre',
+				'campeonato',
+				'equipos'
+			],
+			'verify'	=> [
+				'name',
+				'championship',
+				'teams'
+			],
+		];
+		
+		public function model ($params = []) {
+			if ($params['type'] && $params['tournament']) {
+				$array =  Widgets::multiRenameKey(json_decode(file_get_contents($this->json . '&user=' . $this->user . '&pwd=' . $this->pass . '&metodo=torneos'), true), $this->mappingName['wrong'], $this->mappingName['verify']);
+				foreach ($array as $res) {
+					foreach ($res as $value) {
+						if ($value['key'] == $params['tournament']) {
+							return $this->getTeams($value['division'], $value['championship']);
+						}
+					}
+				}
+			}
+			return null;
+		}
+
+		/**
+		 * Get Teams
+		 * 
+		 * @division int
+		 * @championship int
+		 */
+		private function getTeams ($division, $championship) {
+			$array = Widgets::multiRenameKey(json_decode(file_get_contents($this->json . '&user=' . $this->user . '&pwd=' . $this->pass . '&metodo=equipos&division='. $division .'&campeonato='. $championship), true), $this->mappingName['wrong'], $this->mappingName['verify']);
+			return $array['teams'];
+		}
+	}
+?>
