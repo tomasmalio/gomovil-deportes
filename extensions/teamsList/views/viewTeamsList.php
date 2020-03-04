3adb37fb1e480210525e53d5c1cd58abf5a99e32
Tomás Malio <tomasmalio@gmail.com>
2020-03-03 23:55:14 -0300

    [Templates] Adding new

--- extensions/teamsList/views/viewTeamsList.php
+++ extensions/teamsList/views/viewTeamsList.php
@@ -1,1 +1,24 @@
 <?php print_r($content)?>
+
+<section class="teamslist">
+	<div class="teamlist-content <?php if ($slider) {?>swiper-container<?php }?>">
+		<ul class="list-teams<?php if ($slider) {?>-slider swiper-wrapper<?php } if (isset($content['position']) && $content['position'] == 'horizontal') {?> horizontal<?php }?>">
+			<?php
+				foreach ($content as $team) {
+					if (isset($_SESSION['clientConfig']->sports->football->display_original_image) && $_SESSION['clientConfig']->sports->football->display_original_image) {
+						$team_image = $_SESSION['clientConfig']->sports->football->url_images . Widgets::normalizeString($team['country']) . '/' . Widgets::normalizeString($team['name']) .'.png';
+					} else {
+						$team_image = $score['team_shield'];
+					}
+			?>
+			<li <?php if ($slider) {?>class="swiper-slide"<?php }?>>
+				<a href="<?=$url?>" class="<?php if ((isset($content['position']) && $content['position'] == 'vertical') || !isset($content['position'])) {?>content-league<?php }?>">
+					<div class="<?php if ((isset($content['position']) && $content['position'] == 'vertical') || !isset($content['position'])) {?>team-image<?php } else {?>card-img<?php }?>">
+						<img src="<?=$team_image?>" name="<?=Widgets::normalizeString($team['name']);?>" alt="" title="">
+					</div>
+				</a>
+			</li>
+			<?php }?>
+		</ul>
+	</div>
+</section>
