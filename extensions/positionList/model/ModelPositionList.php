ff01a97bd73b5410e76802a9b5e1dc0b48b38466
Tomás Malio <tomasmalio@gmail.com>
2020-03-06 15:04:56 -0300

    Adding shields for next matches

--- extensions/positionList/model/ModelPositionList.php
+++ extensions/positionList/model/ModelPositionList.php
@@ -78,7 +78,7 @@
 			if ($type == 'liga' || $type == 'league') {
 				return Widgets::multiRenameKey(json_decode(file_get_contents($this->json . '&user=' . $this->user . '&pwd=' . $this->pass . '&metodo=posiciones&division='. $division .'&campeonato='. $championship), true), $this->mappingName['wrong'], $this->mappingName['verify']);
 			} else {
-				$array = [];
+				$array['positions'] = [];
 				$letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'];
 
 				$end = false;
