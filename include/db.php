a3576639bf6a2b9f22d1be81e2fd7e9ec4329dc8
Tomás Malio <tomasmalio@gmail.com>
2020-03-31 16:54:01 -0300

    Wrong solution for characters specials

--- include/db.php
+++ include/db.php
@@ -3,6 +3,6 @@
 	$db = new Db();
 	$db->setUsername('{@Username}');
 	$db->setPassword('{@Password}');
-	$db->setDsn('mysql:dbname={@Dbname};host={@Host};charset=utf8');
+	$db->setDsn('mysql:dbname={@Dbname};host={@Host}');
 	$db->connect();
 ?>
