6800f7103dd9a39fb9cf9e76f6735115538e8b48
Tomás Malio <tomasmalio@gmail.com>
2020-04-05 15:07:56 -0300

    Adding utf8 for titles

--- extensions/nextMatches/views/viewNextMatches.php
+++ extensions/nextMatches/views/viewNextMatches.php
@@ -4,7 +4,6 @@
 	$date = date('Y-m-d');
 
 	// Validate if we want to show a type of soccer and we've matches
-	print_r($nextMatches);
 	if (((isset($nextMatches['football']) && $nextMatches['football']['display']) && $nextMatches['football']['matches'] != null) || ((isset($nextMatches['tennis']) && $nextMatches['tennis']['display']) && $nextMatches['tennis']['matches'] != null)) {
 ?>
 <section class="nextmatches">
