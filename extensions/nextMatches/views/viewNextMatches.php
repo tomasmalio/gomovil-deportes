e4dfc51d9d472cd2866e8e4e97f48377d258c8ab
Tomás Malio <tomasmalio@gmail.com>
2020-08-05 17:30:17 -0300

    Adding default for team image

--- extensions/nextMatches/views/viewNextMatches.php
+++ extensions/nextMatches/views/viewNextMatches.php
@@ -88,11 +88,11 @@
 						
 						$content = @file_get_contents($team_image_local);
 						if ($content === FALSE) { 
-							$team_image_local = $_SESSION['clientConfig']->sports->football->url_images . '/' . 'default.png';
+							$team_image_local = $_SESSION['clientConfig']->sports->football->url_images . 'default.png';
 						}
 						$content = @file_get_contents($team_image_visit);
 						if ($content === FALSE) { 
-							$team_image_visit = $_SESSION['clientConfig']->sports->football->url_images . '/' .'default.png';
+							$team_image_visit = $_SESSION['clientConfig']->sports->football->url_images . 'default.png';
 						}
 					}
 		?>
