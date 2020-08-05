8363cebcf3bcd7b69cb41630276f0a63367bc16d
Tomás Malio <tomasmalio@gmail.com>
2020-08-05 17:21:18 -0300

    Adding default for team image

--- extensions/nextMatches/views/viewNextMatches.php
+++ extensions/nextMatches/views/viewNextMatches.php
@@ -73,37 +73,17 @@
 						$team_image_local = $_SESSION['clientConfig']->sports->football->url_images . Widgets::normalizeString($match['country_local']) . '/' . Widgets::normalizeString($match['team_local']) .'.png';
 						$team_image_visit = $_SESSION['clientConfig']->sports->football->url_images . Widgets::normalizeString($match['country_visit']) . '/' . Widgets::normalizeString($match['team_visit']) .'.png';
 
-						// $ch = curl_init($team_image_local);
-						// curl_setopt($ch, CURLOPT_NOBODY, true);
-						// curl_exec($ch);
-						// $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-						// curl_close($ch);
-
-						// // Check the response code
-						// if ($responseCode != 200){
-						// 	$team_image_local = $_SESSION['clientConfig']->sports->football->url_images . Widgets::normalizeString($match['team_country_local']) . '/' . 'default.png';
-						// }
-
-						// $ch = curl_init($team_image_visit);
-						// curl_setopt($ch, CURLOPT_NOBODY, true);
-						// curl_exec($ch);
-						// $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-						// curl_close($ch);
-
-						// if ($responseCode != 200){
-						// 	$team_image_visit = $_SESSION['clientConfig']->sports->football->url_images . Widgets::normalizeString($match['team_country_visit']) . '/' .'default.png';
-						// }
 
 					} else {
 						$team_image_local = $_SESSION['clientConfig']->sports->football->url_images . Widgets::normalizeString($match['team_local']) .'.png';
 						$team_image_visit = $_SESSION['clientConfig']->sports->football->url_images . Widgets::normalizeString($match['team_visit']) .'.png';
 						$content = @file_get_contents($team_image_local);
 						if ($content === FALSE) { 
-							$team_image_local = $_SESSION['clientConfig']->sports->football->url_images . Widgets::normalizeString($match['team_country_local']) . '/' . 'default.png';
+							$team_image_local = $_SESSION['clientConfig']->sports->football->url_images . '/' . 'default.png';
 						}
 						$content = @file_get_contents($team_image_visit);
 						if ($content === FALSE) { 
-							$team_image_visit = $_SESSION['clientConfig']->sports->football->url_images . Widgets::normalizeString($match['team_country_visit']) . '/' .'default.png';
+							$team_image_visit = $_SESSION['clientConfig']->sports->football->url_images . '/' .'default.png';
 						}
 					}
 		?>
