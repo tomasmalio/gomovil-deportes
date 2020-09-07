c3486efe1b4076b74cd51f9be4f653abf298301a
Tomás Malio <tomasmalio@gmail.com>
2020-09-07 12:38:55 -0300

    Changing menu header

--- templates/header.inc.php
+++ templates/header.inc.php
@@ -81,9 +81,9 @@
 													</div>
 													<h5 class="card-title">
 														{% if each.name[country] is empty %}
-														{{ each.name.default|convert_encoding('UTF-8','UTF-8') }}
+														{{ each.name.default|convert_encoding('ISO-8859-1','UTF-8') }}
 														{% else %}
-														{{ each.name[country]|convert_encoding('UTF-8','UTF-8') }}
+														{{ each.name[country]|convert_encoding('ISO-8859-1','UTF-8') }}
 														{% endif %}
 													</h5>
 												</a>
